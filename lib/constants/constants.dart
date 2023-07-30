b11f2a154ad3c4676b1381f5218fb54f3602d7a3
JasurbekErgashev <jergashev633@gmail.com>
2023-07-30 23:18:08 +0500

    added required project ids to constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+const APPWRITE_PROJECT_ID = '64c6053c79eda5fc59b6';
+const APPWRITE_URL = 'https://cloud.appwrite.io/v1';
+
+const APPWRITE_DATABASE_ID = '64c6a89a591615cd7a2f';
+const COLLECTION_MESSAGES_ID = '64c6a8cec2c545c94b39';
