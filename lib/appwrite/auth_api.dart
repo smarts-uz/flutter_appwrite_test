899845c686773628d4a8033e5b641cc34f1fcd78
JasurbekErgashev <jergashev633@gmail.com>
2023-07-31 00:01:21 +0500

    added appwrite starting code

--- lib/appwrite/auth_api.dart
+++ lib/appwrite/auth_api.dart
@@ -1,29 +1,51 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
+import 'package:appwrite_test/constants/constants.dart';
 import 'package:flutter/material.dart';
 
-class AppwriteClient {
-  static Client client = Client();
+enum AuthStatus {
+  uninitialized,
+  authenticated,
+  unauthenticated,
+}
 
-  Account account = Account(client);
+class AuthApi extends ChangeNotifier {
+  Client client = Client();
+  late final Account account;
+  late User _currentUser;
+  AuthStatus _status = AuthStatus.uninitialized;
 
-  // Register User
-  Future<User?> signup({
-    required String email,
-    required String password,
-    String? name,
-  }) async {
+  /// Getters
+  User get user => _currentUser;
+  AuthStatus get status => _status;
+  String get userName => _currentUser.name;
+  String get email => _currentUser.email;
+  String get userId => _currentUser.$id;
+
+  AuthApi() {
+    init();
+    loadUser();
+  }
+
+  init() {
+    client
+        .setEndpoint(APPWRITE_URL)
+        .setProject(APPWRITE_PROJECT_ID)
+        .setSelfSigned();
+    Account(client);
+  }
+
+  loadUser() async {
     try {
-      final user = await account.create(
-        userId: ID.unique(),
-        email: email,
-        password: password,
-        name: name,
-      );
-      return user;
-    } on AppwriteException catch (e) {
-      debugPrint(e.toString());
+      final user = await account.get();
+      _status = AuthStatus.authenticated;
+      _currentUser = user;
+    } catch (e) {
+      _status = AuthStatus.unauthenticated;
+    } finally {
+      notifyListeners();
     }
-    return null;
   }
 }
